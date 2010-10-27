a3fa273dcbc38589a620dd852247c6c4a16b9f6d
melf <melf.common@gmail.com>
2010-10-27 16:54:59 +0200

    Initial Commit
    Signed-off-by: melf

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace VertGird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(676, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 191);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(676, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(546, 348);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(116, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://melf.eloxion.com";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 399);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Vertical Grid, you might need it one day";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VertGird
+{
+    public partial class Form1 : Form
+    {
+        DataTable tbl = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load Datatable, remember this can be your dataset from your DB.
+            //I don't create DBs I'm not a DBA  
+            this.tbl = GetCoutries();
+            LoadNomalGrid();
+            LoadVerticalGrid();
+
+            //lets cheat to hide column header on vertical grid
+            this.dataGridView2.ColumnHeadersVisible = false;
+
+        }
+
+        /// <summary>
+        /// We're going to have some nested loops here.
+        /// </summary>
+        private void LoadVerticalGrid()
+        {
+            //first lets create a new table which will have the new vertical structure
+            DataTable VerticalTable = new DataTable();
+
+            //get number of rows to make new columns
+            for (int i = 0; i < this.tbl.Rows.Count; i++)
+            {
+                VerticalTable.Columns.Add();
+            }
+            DataRow row;
+            //New rows for the columns
+            for (int j = 0; j < this.tbl.Columns.Count; j++)
+            {
+
+                row = VerticalTable.NewRow();
+                row[0] = this.tbl.Columns[j].ToString();// set first field of the row to be coulmn fro the table
+                for (int k = 1; k < this.tbl.Rows.Count; k++)
+                {
+
+                    row[k] = this.tbl.Rows[k - 1][j];//noticed the -1 on the rows count
+                }
+                VerticalTable.Rows.Add(row);
+            }
+
+            VerticalTable.AcceptChanges();
+            this.dataGridView2.DataSource = VerticalTable.DefaultView;
+
+        }
+
+        private void LoadNomalGrid()
+        {
+            this.dataGridView1.DataSource = this.tbl.DefaultView;
+        }
+
+        private DataTable GetCoutries()
+        {
+            DataTable table = new DataTable("Coutries");
+            //Add Cloumns
+            table.Columns.Add("Country");
+            table.Columns.Add("Currency");
+            table.Columns.Add("Population");
+            table.Columns.Add("President");
+
+            //Populate dummy data
+            table.Rows.Add(new object[] { "South Africa", "ZAR", "48 Mil", "Comrade JZ" });
+            table.Rows.Add(new object[] { "USA", "USD", "250 Mil", "Obama" });
+            table.Rows.Add(new object[] { "Zim", "ZMD", "20 Mil", "Uncle Bob" });
+            table.Rows.Add(new object[] { "Nigeria", "Naira", "200 Mil", "Goodluck Jonathan" });
+
+            table.AcceptChanges();
+
+            return table;
+        }
+
+     
+
+       
+
+      
+    }
+}
